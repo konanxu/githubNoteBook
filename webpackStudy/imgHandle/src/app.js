a978814a8650ac004c72b2e07451bd8f002258c9
Konan <konan@KonandeMacBook-Pro.local>
2018-08-02 16:05:29 +0800

    img处理

---
+++ webpackStudy/imgHandle/src/app.js
@@ -0,0 +1,11 @@
+// import common from './css/common.scss'
+import base from './css/base.scss'
+
+
+// var app = document.getElementById('app')
+// app.innerHTML = '<div class="'+ base.box +'"><div class="' + base.ani1 + '"></div></div>'
+
+// // 动态异步加载
+// import(/* webpackChunkName: 'a' */ './components/a').then(a =>{
+//   console.log(a)
+// })
---
+++ webpackStudy/imgHandle/webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path')
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist/',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextWebpackPlugin.extract({
+          fallback: {
+            loader: 'style-loader',
+            // options: {
+            //   // insertInto: '#app',
+            //   singleton: true,
+            //   // transform: './css.transform.js'
+            // }
+          },
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                // minimize: true,
+                // sourceMap: true,
+                modules: true,
+                // localIdentName: '[path][name]_[local]_[hash:base64:5]'
+                localIdentName: '[local]'
+              }
+            },
+            {
+              loader:'sass-loader'
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(png|jpg|jepg|gif)$/,
+        use: [
+          // {
+          //   loader: 'file-loader',
+          //   options: {
+          //     // outputPath: 'dist/',
+          //     publicPath: '',
+          //     useRelativePath: true
+          //   }
+          // }
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,  //limit值小则为路径 , 超过base64大小设置则 转换 base64
+              // outputPath: 'asset/img/',
+              publicPath: './assets/imgs/',
+              useRelativePath: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin({
+      filename: '[name].min.css',
+      allChunks: true
+    })
+  ]
+}
